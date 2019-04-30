2f07cb417878d03103307bcd7d53ea09bb352c23
Howard Moore <qsshorty@gmail.com>
2019-04-30 15:54:22 -0700

    google news feed

---
+++ api.js
@@ -0,0 +1,6 @@
+$(document).ready(initializeApp);
+
+function initializeApp() {
+    var gn = new GoogleNews();
+    gn.getData();
+}
---
+++ src/google-news.js
@@ -0,0 +1,38 @@
+
+
+class GoogleNews {
+    constructor() {
+    this.getData = this.getData.bind(this);
+    this.newsArticle = this.newsArticle.bind(this);
+    }
+
+    getData() {
+        $.ajax({
+            url: 'https://newsapi.org/v2/everything?apiKey=4875328af25d48efad522d7dee93b271&q=lakers',
+            method: 'get',
+            dataType: 'json',
+            success: this.newsArticle.bind(this)
+        });
+
+    }
+    newsArticle(response) {
+        console.log(response.articles);
+        //this.data = response;
+        for (var index = 0; index < 11; index++){ //response.articles.length
+            let title = response.articles[index].title;
+            let imgLink = response.articles[index].urlToImage;
+            let imgInfo = $('<img>').attr('src', imgLink);
+            let link = response.articles[index].url;
+
+            let container = $('<div>');
+            let atag = $('<a>').attr('href', link).attr('target', '_blank');
+            let text = $('<div>').text(response.articles[index].title);
+            let linkedtext = $(atag).append(text);
+            $(container).append(imgInfo).append(linkedtext);
+            $('.news-feed').append(container);
+        }
+
+    }
+
+}
+
