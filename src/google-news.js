5d44d68173ea2e6d90b812a9962ac7b13051d45e
Kevin Yang <kyang812@yahoo.com>
2019-05-02 18:00:07 -0700

    used google geolocation to search events based on location

--- src/google-news.js
+++ src/google-news.js
@@ -4,7 +4,7 @@ class GoogleNews {
     constructor() {
     this.getData = this.getData.bind(this);
     this.newsArticle = this.newsArticle.bind(this);
-    this.currentLocation = this.currentLocation.bind(this);
+    // this.currentLocation = this.currentLocation.bind(this);
     }
 
     getData(param) {
@@ -17,7 +17,6 @@ class GoogleNews {
 
     }
     newsArticle(response) {
-        //console.log(response.articles);
         for (var index = 0; index < response.articles.length; index++){ //response.articles.length
             let title = response.articles[index].title;
             let imgLink = response.articles[index].urlToImage;
@@ -33,16 +32,7 @@ class GoogleNews {
         }
 
     }
-    currentLocation() {
-        $.ajax({
-            url: 'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDMoJiBDxdSzHNulmyQTcwhvN7XUErfXBo',
-            method: 'post',
-            dataType: 'json',
-            success: function (response) {
-                console.log(response);
-            }
-        });
-    }
+    
 
 }
 
